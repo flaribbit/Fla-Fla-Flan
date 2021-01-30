69a5ff1f70bf3d1ee97a07fc6a065bbd2aa72d0d
flaribbit <1149761294@qq.com>
2021-01-30 11:12:15 +0800

    绑定class

--- game.js
+++ game.js
@@ -1,6 +1,8 @@
 var game = new Vue({
     el: '#maingame',
     data: {
+        data_tr: ["translate(-290 -102)", "translate(-230 -80)", "translate(-170 -58)", "translate(-110 -36)", "translate(290 -102)", "translate(230 -80)", "translate(170 -58)", "translate(110 -36)"],
+        data_cl: ["red", "yellow", "green", "blue", "orange", "cyan", "purple", "black"],
         wing: [[], [], [], [], [], [], [], []],
         skirt: 0,
         body: 0,
@@ -16,7 +18,7 @@ function init() {
     for (var i = 0; i < 8; i++) {
         var col = [];
         for (var j = 0; j < 4; j++) {
-            col.push(Math.floor(6 * Math.random()) + 1);
+            col.push(Math.floor(6 * Math.random()));
         }
         wing.push(col);
     }
